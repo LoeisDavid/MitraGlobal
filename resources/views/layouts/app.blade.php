b993e943375a99196ffcf335a3a23b2a139824c6
XiaooPouu <pranswrld@gmail.com>
2025-12-29 21:01:13 +0700

    Membuat layouts untuk mempermudah dalam membuat tampilan, Memasukkan adminlte 3 sebagai tampilan yang akan digunakan, Membuat includes yang digunakan untuk memisah bagian header footer dan navbar menjadi bagian tersendiri

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title', 'AdminLTE 3')</title>
+
+    {{-- Font Awesome --}}
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+
+    {{-- Tempusdominus Bootstrap 4 --}}
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+
+    {{-- AdminLTE --}}
+    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+
+    {{-- Google Font: Source Sans Pro --}}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        {{-- Navbar --}}
+        @include('includes.navbar')
+
+        {{-- Sidebar --}}
+        @include('includes.sidebar')
+
+        {{-- Content --}}
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">@yield('page-title')</h1>
+                    </div><!-- /.col -->
+                    <!-- Breadcrumb -->
+                    <div class="col-sm-6">
+                        @yield('breadcrumb')
+                    </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+                <!-- /.content-header -->
+
+            <section class="content">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </section>
+        </div>
+        {{-- End Content --}}
+
+        {{-- Footer --}}
+        @include('includes.footer')
+    </div>
+
+{{-- Scripts --}}
+<script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+</body>
+</html>
